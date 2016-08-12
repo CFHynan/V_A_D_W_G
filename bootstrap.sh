be049de5e05ceed8103938308b31cababefab878
Chris Hynan <chynan@intzure.com>
2016-08-12 17:47:39 +0100

    Install Ansible on control machine.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Install Ansible.
+
+if [ ! -f /usr/bin/ansible-playbook ]
+then
+  apt-get install software-properties-common
+  apt-add-repository ppa:ansible/ansible
+  apt-get update
+  apt-get install -y ansible
+fi
+
+# Install Apache Web Server.   
+
 #apt-get update
 #apt-get install -y apache2
 #if ! [ -L /var/www ]; then
