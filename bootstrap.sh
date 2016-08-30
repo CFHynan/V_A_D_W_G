5da54c567eeda7de3c0d6fe9a3c58f47d8225c44
Chris Hynan <chynan@intzure.com>
2016-08-30 21:14:46 +0100

    Ansible config.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 
 chmod 600 /home/vagrant/.ssh/id_rsa
 
-# Install Ansible.
+# Install and configure Ansible.
 
 if [ ! -f /usr/bin/ansible-playbook ]
 then
@@ -12,8 +12,11 @@ then
 #  For >= v12.10, otherwise use python below: apt-get install -y software-properties-common
   apt-get install -y python-software-properties
   apt-add-repository -y ppa:ansible/ansible
+  apt-get --purge remove -y ansible
   apt-get install -y ansible
 fi
+
+echo -e "10.0.0.5\n" >> /etc/ansible/hosts
 
 # Install Apache Web Server.   
 
